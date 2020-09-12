20d66fd0355ca4585248ef9cf7f85594d67dfec0
Padma-1 <65107839+Padma-1@users.noreply.github.com>
2020-09-12 18:21:25 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -11,12 +11,18 @@ for i in range(row):
         y.append(randint(1,100))
     x.append(y)
     
-for i in x:
+"""for i in x:
     for j in i:
         print("%3d" %j,end = ' ')#%3d due to generating gap between row & column
-    print()
+    print()"""
+for y in x:
+    print(y)
 
 ## output:
 ## 48  88  93 
 ## 30  81  35 
 ## 26  90  47 
+
+#[22, 18, 63]
+#[58, 42, 30]
+#[31, 77, 70]
